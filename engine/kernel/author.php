d03729579b10f7ba9030617f2c8ad709bae46682
Taufik Nurrohman <hi@taufik-nurrohman.com>
2026-03-10 09:28:28 +0700

    Update

--- engine/kernel/author.php
+++ engine/kernel/author.php
@@ -11,8 +11,10 @@ class Author extends User {
 
     public function route(...$lot) {
         if (0 === strpos($this->path ?? D, LOT . D . 'user' . D)) {
+            if (!is_string($name = $this->name())) {
+                return null;
+            }
             extract(lot(), EXTR_SKIP);
-            $name = $this->name;
             $parent = $this->parent;
             return ($parent ? $parent->route : "") . '/' . trim($state->x->author->route ?? 'author', '/') . '/' . $name . ($parent ? '/1' : "");
         }
--- engine/kernel/authors.php
+++ engine/kernel/authors.php
@@ -3,6 +3,12 @@
 class Authors extends Users {
 
     public function page(...$lot) {
+        if (($v = reset($lot)) && $v instanceof Author) {
+            return $v;
+        }
+        if (is_array($v) && isset($v["\0"])) {
+            return $v["\0"];
+        }
         return new Author(...$lot);
     }
 
